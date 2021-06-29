ae8c15326b444ab764345d7b17d93233fc4ab049
ValentinaTarapow <tarapow.v@gmail.com>
2021-06-28 22:41:30 -0300

    form

---
+++ pages/enviar.php
@@ -0,0 +1,25 @@
+<?php
+// $nombre = $_POST['nombre'];
+// $email = $_POST['email'];
+// $motivo = $_POST['motivo'];
+// $comentario = $_POST['comentario'];
+// $newsletter = $_POST['newsletter'];
+
+// $header = 'From: ' . $email . " \r\n";
+// $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+// $header .= "Mime-Version: 1.0 \r\n";
+// $header .= "Content-Type: text/plain";
+// ​
+// $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+// $mensaje .= "Su e-mail es: " . $email . " \r\n";
+// $mensaje .= "Mensaje: " . $_POST['comentario'] . " \r\n";
+// $mensaje .= "Enviado el " . date('d/m/Y', time());
+// ​
+// $para = 'tarapow.v@gmail.com'; // El mail a donde van a llegar los mensajes
+// $asunto = 'Mensaje de mi sitio web';
+// ​
+// mail($para, $asunto, utf8_decode($mensaje), $header);
+// ​
+// header('Location:index.html');
+
+?>
